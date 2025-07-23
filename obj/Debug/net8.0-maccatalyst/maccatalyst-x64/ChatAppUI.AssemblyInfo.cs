f9538c6df90e583f4b898f3ea861e84174159d5d
99FAlex <127439399+99FAlex@users.noreply.github.com>
2025-07-23 17:34:49 +0200

    Update

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ChatAppUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ChatAppUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+12e58f074dd96b77d34453f8044c110c59fad11e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+21a66655d7e45a50284e830c2f8a5208ae31566d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
